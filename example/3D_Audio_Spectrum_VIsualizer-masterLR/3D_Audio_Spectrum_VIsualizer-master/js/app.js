88d74d2f82be692072948f93dad21820d311216f
Sam Rolfes <sam@jointhestudio.com>
2015-07-07 13:22:55 -0400

    changed .obj link

--- example/3D_Audio_Spectrum_VIsualizer-masterLR/3D_Audio_Spectrum_VIsualizer-master/js/app.js
+++ example/3D_Audio_Spectrum_VIsualizer-masterLR/3D_Audio_Spectrum_VIsualizer-master/js/app.js
@@ -353,7 +353,7 @@ Visualizer.prototype = {
         			
                   var objLoader = new THREE.OBJLoader();
                var material = new THREE.MeshBasicMaterial({color: 'yellow', side: THREE.DoubleSide});
-               objLoader.load('sources/teengirl3.OBJ', function (obj) {
+               objLoader.load('samrolfes.github.io/geoVisualizer/otherWayAround/objects/teengirl3.OBJ', function (obj) {
                    obj.traverse(function (child) {
 
                        if (child instanceof THREE.Mesh) {
